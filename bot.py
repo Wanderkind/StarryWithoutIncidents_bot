e37060fcafee097338cc5f0a74a9933dcd951724
Jeongseop Lee <wanderkind@semyung.ac.kr>
2025-02-10 22:35:02 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -95,7 +95,7 @@ async def set_daily_update(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Schedule a new job
     job_queue.run_daily(
         send_daily_status, 
-        time=datetime.time(hour=22, minute=33), 
+        time=datetime.time(hour=22, minute=36), 
         chat_id=chat_id, 
         name=str(chat_id)
     )
